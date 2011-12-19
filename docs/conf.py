12a0cfc157dd595f691a94068b9ee4cb0b14cce1
holgerd77 <Holger.Drewes@googlemail.com>
2011-12-19 17:32:38 +0100

    Minor docs changes

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2011, Holger Drewes'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.0'
+version = '0.1'
 # The full version, including alpha/beta/rc tags.
 release = '0.1.0-pre-alpha'
 
