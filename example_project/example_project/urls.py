92a535947d0032b4e3b5187e26b43704cd5c0ad4
holgerd77 <Holger.Drewes@googlemail.com>
2012-06-22 15:52:08 +0200

    Adopted example project to Django 1.4 project layout

---
+++ example_project/example_project/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'example_project2.views.home', name='home'),
+    # url(r'^example_project2/', include('example_project2.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
