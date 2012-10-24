eca42bd4156ddaf385d288709435ba800a61a3a2
holgerd77 <Holger.Drewes@googlemail.com>
2012-10-24 15:38:13 +0200

    Experiments with setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 import os
 
@@ -12,6 +12,7 @@ setup(
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     license='BSD License',
     platforms=['OS Independent'],
+    include_package_data=True,
     packages=[
         'dynamic_scraper',
         'dynamic_scraper.spiders',
