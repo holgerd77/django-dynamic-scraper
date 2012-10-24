9772586f0a88c99f4ff5b0533fb20a86998b0808
holgerd77 <Holger.Drewes@googlemail.com>
2012-10-24 15:50:34 +0200

    Experiments with setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from distutils.core import setup
 
 import os
 
 setup(
     name='django-dynamic-scraper',
-    version='0.2',
+    version='0.2.3',
     description='Creating Scrapy scrapers via the Django admin interface',
     author='Holger Drewes',
     author_email='Holger.Drewes@gmail.com',
@@ -12,22 +12,21 @@ setup(
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     license='BSD License',
     platforms=['OS Independent'],
-    include_package_data=True,
     packages=[
         'dynamic_scraper',
         'dynamic_scraper.spiders',
         'dynamic_scraper.utils',
         'dynamic_scraper.migrations',
     ],
-    install_requires=[
-        'Django>=1.3.1',
-        'Scrapy>=0.14.0,<0.16.0',
-        
-        'celery>=2.5.0,<3.0', # Scheduling
-        'django-celery>=2.5.0,<3.0',
-        'PIL',
-        'South',
-    ],
+    #install_requires=[
+    #    'Django>=1.3.1',
+    #    'Scrapy>=0.14.0,<0.16.0',
+    #    
+    #    'celery>=2.5.0,<3.0', # Scheduling
+    #    'django-celery>=2.5.0,<3.0',
+    #    'PIL',
+    #    'South',
+    #],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
