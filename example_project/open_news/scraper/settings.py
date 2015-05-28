fd7eb6a14ecf974fad8defdbcd427480ac99ae44
holgerd77 <Holger.Drewes@googlemail.com>
2015-05-28 13:36:20 +0200

    Added dynamic_scraper python path for Scrapy execution in example_project

--- example_project/open_news/scraper/settings.py
+++ example_project/open_news/scraper/settings.py
@@ -6,10 +6,11 @@
 #     http://doc.scrapy.org/topics/settings.html
 #
 
-import os
+import os, sys
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_project.settings")
+sys.path.insert(0, os.path.join(PROJECT_ROOT, "../../..")) #only for example_project
 
 
 BOT_NAME = 'open_news'
