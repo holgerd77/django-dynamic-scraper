27d0411ae7f7ab591a59950aaec4fd8ca4436e44
holgerd77 <Holger.Drewes@googlemail.com>
2015-07-14 11:28:26 +0200

    Updated open_news.json fixture from example project with new DB structure (ID fields added)

--- example_project/open_news/models.py
+++ example_project/open_news/models.py
@@ -19,7 +19,7 @@ class Article(models.Model):
     title = models.CharField(max_length=200)
     news_website = models.ForeignKey(NewsWebsite) 
     description = models.TextField(blank=True)
-    url = models.URLField()
+    url = models.URLField(blank=True)
     thumbnail = models.CharField(max_length=200, blank=True)
     checker_runtime = models.ForeignKey(SchedulerRuntime, blank=True, null=True, on_delete=models.SET_NULL)
     
