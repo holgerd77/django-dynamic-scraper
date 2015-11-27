7c43710c0c1948ac82c30f219aa136f0cd0a48c7
holgerd77 <Holger.Drewes@googlemail.com>
2015-11-27 11:14:47 +0100

    Python 3 compatibility for task_utils.py

--- dynamic_scraper/utils/task_utils.py
+++ dynamic_scraper/utils/task_utils.py
@@ -1,6 +1,9 @@
+#Stage 2 Update (Python 3)
+from future import standard_library
+standard_library.install_aliases()
 from builtins import object
 import datetime, json
-import urllib, urllib2, httplib
+import urllib.request, urllib.parse, http.client
 from scrapy.utils.project import get_project_settings
 settings = get_project_settings()
 from dynamic_scraper.models import Scraper
@@ -20,16 +23,16 @@ class TaskUtils(object):
             'run_type': kwargs['run_type'],
             'do_action': kwargs['do_action']
         }
-        params = urllib.urlencode(param_dict)
+        params = urllib.parse.urlencode(param_dict)
         headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-        conn = httplib.HTTPConnection("localhost:6800")
+        conn = http.client.HTTPConnection("localhost:6800")
         conn.request("POST", "/schedule.json", params, headers)
         conn.getresponse()
     
     
     def _pending_jobs(self, spider):
         # Ommit scheduling new jobs if there are still pending jobs for same spider
-        resp = urllib2.urlopen('http://localhost:6800/listjobs.json?project=default')
+        resp = urllib.request.urlopen('http://localhost:6800/listjobs.json?project=default')
         data = json.load(resp)
         if 'pending' in data:
             for item in data['pending']:
@@ -60,9 +63,6 @@ class TaskUtils(object):
         }
         for key in kwargs:
             filter_kwargs[key] = kwargs[key]
-         
-        #for key in filter_kwargs:
-        #    print "another keyword arg: %s: %s" % (key, filter_kwargs[key])
         
         exclude_kwargs = {
             '%s__checker_type' % scraper_field_path: 'N',
