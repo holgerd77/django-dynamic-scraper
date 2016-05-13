9ad893d20c81364a12f7793ba9f4d0d5598b6219
holgerd77 <Holger.Drewes@googlemail.com>
2016-05-13 15:41:59 +0200

    Release commit for v.0.11.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name='django-dynamic-scraper',
-    version='0.10.1',
+    version='0.11.0',
     description='Creating Scrapy scrapers via the Django admin interface',
     author='Holger Drewes',
     author_email='Holger.Drewes@gmail.com',
