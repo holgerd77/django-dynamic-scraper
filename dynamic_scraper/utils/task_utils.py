232088c1231b24874bdfac4d828e3bbe033114bd
Umair Ashraf <umrashrf@gmail.com>
2019-10-03 12:10:04 -0400

    replaced more localhost with hostname from env

--- dynamic_scraper/utils/task_utils.py
+++ dynamic_scraper/utils/task_utils.py
@@ -18,6 +18,9 @@ class TaskUtils(object):
         "MAX_CHECKER_RUNS_PER_TASK": 25,
     }
 
+    def __init__(self, *args, **kwargs):
+        super(TaskUtils, self).__init__(*args, **kwargs)
+        self._scrapyd_base_url = settings.get('SCRAPYD_URL', "localhost:{}".format(scrapyd_port))
 
     def _run_spider(self, **kwargs):
         param_dict = {
@@ -29,15 +32,14 @@ class TaskUtils(object):
         }
         params = urllib.parse.urlencode(param_dict)
         headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-        scrapyd_url = settings.get('SCRAPYD_URL', "localhost:{}".format(scrapyd_port))
-        conn = http.client.HTTPConnection(scrapyd_url)
+        conn = http.client.HTTPConnection(self._scrapyd_base_url)
         conn.request("POST", "/schedule.json", params, headers)
         conn.getresponse()
 
 
     def _pending_jobs(self, spider):
         # Ommit scheduling new jobs if there are still pending jobs for same spider
-        resp = urllib.request.urlopen('http://localhost:{}/listjobs.json?project=default'.format(scrapyd_port))
+        resp = urllib.request.urlopen('{}/listjobs.json?project=default'.format(self._scrapyd_base_url))
         data = json.loads(resp.read().decode('utf-8'))
         if 'pending' in data:
             for item in data['pending']:
